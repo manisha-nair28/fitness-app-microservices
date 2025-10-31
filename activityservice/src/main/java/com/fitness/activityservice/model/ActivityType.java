e1878267968c7ad9fcd49145d62c4131dd76b78c
Manisha Nair <manishanairnair2002@gmail.com>
2025-10-31 21:33:21 +0530

    First

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,17 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    JOGGING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    YOGA,
+    PILATES,
+    CARDIO,
+    WEIGHT_TRAINING,
+    HIIT,
+    STRETCHING,
+    ZUMBA,
+    OTHERS
+}
---
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -0,0 +1,49 @@
+package com.fitness.aiservice.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Service
+public class GeminiService {
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public GeminiService(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.build();
+    }
+
+    public String getAnswer(String question){
+
+        //constructing the API request body exactly as per Gemini API format
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[]{
+                        Map.of("parts", new Object[]{
+                                Map.of("text", question)
+                        })
+                }
+        );
+
+        // Send POST request with proper headers and URL format
+        String response = webClient.post()
+                .uri(geminiApiUrl)
+                .header("x-goog-api-key", geminiApiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println("Response is as follows : " + response);
+
+        return response;
+    }
+}
